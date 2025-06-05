ac1d33c412c39a57dfac8014fedeedb8bf19fe7c
shehab99b <shehabulislam429@gmail.com>
2025-06-05 23:52:40 +0600

    The setUp part is here

---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import Home from "../Home/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children: [
+            {
+                index: true,
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default router
